b5d8f97f403cefc32a6104d0738dbce8911558dc
ifs-vincentrehm <vincent.rehm@ifsalpha.com>
2020-05-19 18:23:50 +0200

    added js for navbar + style config

--- components/TheNavbar.vue
+++ components/TheNavbar.vue
@@ -4,15 +4,15 @@
       <div class="relative flex justify-between h-16">
         <div class="absolute inset-y-0 left-0 flex items-center sm:hidden">
           <!-- Mobile menu button -->
-          <button class="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 focus:text-gray-500 transition duration-150 ease-in-out" aria-label="Main menu" aria-expanded="false">
+          <button class="inline-flex items-center justify-center p-2 rounded-md text-blue-600 hover:text-blue-700 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 focus:text-blue-700 transition duration-150 ease-in-out" aria-label="Main menu" aria-expanded="false" @click="isOpen = !isOpen">
             <!-- Icon when menu is closed. -->
             <!-- Menu open: "hidden", Menu closed: "block" -->
-            <svg class="block h-6 w-6" stroke="currentColor" fill="none" viewBox="0 0 24 24">
+            <svg :class="isOpen ? 'hidden' : 'block'" class="h-6 w-6" stroke="currentColor" fill="none" viewBox="0 0 24 24">
               <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
             </svg>
             <!-- Icon when menu is open. -->
             <!-- Menu open: "block", Menu closed: "hidden" -->
-            <svg class="hidden h-6 w-6" stroke="currentColor" fill="none" viewBox="0 0 24 24">
+            <svg :class="isOpen ? 'block' : 'hidden'" class="h-6 w-6" stroke="currentColor" fill="none" viewBox="0 0 24 24">
               <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
             </svg>
           </button>
@@ -27,7 +27,7 @@
           </div>
 
           <div class="hidden sm:ml-6 sm:flex">
-            <a href="#" class="navbar-first-link focus:outline-none focus:border-indigo-700">
+            <a href="#" class="navbar-first-link focus:outline-none focus:border-blue-700">
               {{ links[0] }}
             </a>
 
@@ -44,9 +44,9 @@
 
     Menu open: "block", Menu closed: "hidden"
   -->
-    <div class="hidden sm:hidden">
+    <div :class="isOpen ? 'block' : 'hidden sm:hidden'">
       <div class="pt-2 pb-4">
-        <a href="#" class="navbar-mobile-first-link focus:outline-none focus:text-indigo-800 focus:bg-indigo-100 focus:border-indigo-700">
+        <a href="#" class="navbar-mobile-first-link focus:outline-none focus:text-blue-800 focus:bg-blue-100 focus:border-blue-700">
           {{ links[0] }}
         </a>
         <a v-for="(link, index) in filteredLinks" :key="index" href="#" class="navbar-mobile-second-links hover:text-gray-800 hover:bg-gray-50 hover:border-gray-300 focus:outline-none focus:text-gray-800 focus:bg-gray-50 focus:border-gray-300">
@@ -60,7 +60,8 @@
 <script>
 export default {
   data: () => ({
-    links: ['Blog', 'Nos logements', 'Le coliving ?']
+    links: ['Blog', 'Nos logements', 'Le coliving ?'],
+    isOpen: false
   }),
   computed: {
     filteredLinks() {
@@ -72,7 +73,7 @@ export default {
 
 <style>
 .navbar-first-link {
-  @apply inline-flex items-center px-1 pt-1 border-b-2 border-indigo-500 text-lg font-medium leading-5 text-gray-900 transition duration-150 ease-in-out;
+  @apply inline-flex items-center px-1 pt-1 border-b-2 border-blue-500 text-lg font-medium leading-5 text-gray-900 transition duration-150 ease-in-out;
 }
 
 .navbar-second-links {
@@ -80,7 +81,7 @@ export default {
 }
 
 .navbar-mobile-first-link {
-  @apply block pl-3 pr-4 py-2 border-l-4 border-indigo-500 text-base font-medium text-indigo-700 bg-indigo-50 transition duration-150 ease-in-out;
+  @apply block pl-3 pr-4 py-2 border-l-4 border-blue-500 text-base font-medium text-blue-700 bg-blue-50 transition duration-150 ease-in-out;
 }
 
 .navbar-mobile-second-links {
