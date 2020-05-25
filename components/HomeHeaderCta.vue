a71cc3bd90a0b973e5c4dfc63e566b40268c0793
ifs-vincentrehm <vincent.rehm@ifsalpha.com>
2020-05-25 17:44:26 +0200

    add lazyloading for images

--- components/HomeHeaderCta.vue
+++ components/HomeHeaderCta.vue
@@ -1,8 +1,8 @@
 <template>
   <section id="header" class="relative">
     <div class="relative flex justify-center">
-      <img class="block sm:hidden" src="~/assets/img/header-mobile.jpg" alt="Découvrez nos logements en coliving" />
-      <img class="hidden sm:block" src="~/assets/img/header-desktop.jpg" alt="Découvrez nos logements en coliving" />
+      <img class="block sm:hidden lazyload" data-src="~/assets/img/header-mobile.jpg" alt="Découvrez nos logements en coliving" />
+      <img class="hidden sm:block lazyload" data-src="~/assets/img/header-desktop.jpg" alt="Découvrez nos logements en coliving" />
     </div>
     <h1 class="text-white absolute inset-0 h-full uppercase">
       <span class="flex h-3/4 items-center justify-center text-center text-xl px-16 sm:px-8 leading-normal"> Pourquoi louer un studio petit & cher ?<br />découvrez le coliving </span>
