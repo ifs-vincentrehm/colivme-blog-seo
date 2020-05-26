bcd54f9ad12ad82145699a63f7c7abf0a6cff502
ifs-vincentrehm <vincent.rehm@ifsalpha.com>
2020-05-26 22:00:42 +0200

    v1 hardcoded

--- components/TheNavbar.vue
+++ components/TheNavbar.vue
@@ -1,7 +1,7 @@
 <template>
   <nav class="bg-white shadow">
     <div class="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
-      <div class="relative flex justify-between h-16">
+      <div class="relative flex justify-between h-20">
         <div class="absolute inset-y-0 left-0 flex items-center sm:hidden">
           <!-- Mobile menu button -->
           <button class="inline-flex items-center justify-center p-2 rounded-md text-blue-600 hover:text-blue-700 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 focus:text-blue-700 transition duration-150 ease-in-out" aria-label="Main menu" aria-expanded="false" @click="isOpen = !isOpen">
@@ -22,8 +22,7 @@
         <div class="flex-1 flex items-center justify-center sm:items-stretch sm:justify-end">
           <!-- Colivme logo -->
           <div class="flex-shrink-0 flex flex-none lg:flex-1 items-center">
-            <img class="block lg:hidden h-8 w-auto lazyload" :data-src="require('~/assets/img/logos/logo-colivme-web.svg')" alt="Workflow logo" />
-            <img class="hidden lg:block h-8 w-auto lazyload" :data-src="require('~/assets/img/logos/logo-colivme-web.svg')" alt="Workflow logo" />
+            <img class="h-12 w-auto lazyload" :data-src="require('~/assets/img/logos/logo-colivme-web.svg')" alt="Colivme logo" />
           </div>
 
           <div class="hidden sm:ml-6 sm:flex">
