f415627e329af7c516817a5dad0ae3bcef7260be
ifs-vincentrehm <vincent.rehm@ifsalpha.com>
2020-06-03 18:44:19 +0200

    poc article show

---
+++ components/ArticleShow.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="my-8">
+    <div class="relative w-full">
+      <img :src="article.image.url" :alt="article.title" class="lazyload" />
+      <div class="absolute bottom-0 left-0 bg-opacity-50 bg-blue-800 pt-2 pb-1">
+        <span class="text-white mx-4 capitalize">
+          {{ article.image.name }}
+        </span>
+      </div>
+    </div>
+    <div class="text-gray-700 text-base break-normal my-4 leading-loose">
+      {{ article.content }}
+    </div>
+    <!-- <div class="flex justify-center items-end my-4">
+      <span v-for="category in article.categories" :key="category.id" class="inline-block bg-gray-200 rounded-full text-sm font-semibold text-gray-700 m-1 py-1 px-2">
+        <article-category-pill :name="category.name" :slug="category.slug" />
+      </span>
+    </div> -->
+  </div>
+</template>
+
+<script>
+export default {
+  //   components: {
+  //     ArticleCategoryPill
+  //   },
+  props: {
+    article: {
+      type: Object,
+      required: true
+    }
+  }
+}
+</script>
+
+<style>
+#article-image-title {
+  background-color: #233a49;
+}
+</style>
---
+++ pages/articles/_slug.vue
@@ -0,0 +1,25 @@
+<template>
+  <article-show :article="article" />
+</template>
+
+<script>
+import ArticleShow from '~/components/ArticleShow.vue'
+
+export default {
+  components: {
+    ArticleShow
+  },
+  async asyncData({ $axios, params }) {
+    const {
+      defaults: { baseURL }
+    } = $axios
+    const slug = params.slug
+    let article = await $axios.$get(baseURL + '/articles/?slug=' + slug)
+    const slugifiedArticle = article[0]
+    article = slugifiedArticle
+    return { article }
+  }
+}
+</script>
+
+<style></style>
